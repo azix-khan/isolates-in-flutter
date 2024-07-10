c11d3ded40a2216b5c7f31a7aaccafc02949e0e3
azix khan <azixkhan.55@gmail.com>
2024-07-10 15:42:46 +0500

    now working on isolates

--- lib/isolates_screen.dart
+++ lib/isolates_screen.dart
@@ -10,6 +10,28 @@ class IsolatesScreen extends StatefulWidget {
 class _IsolatesScreenState extends State<IsolatesScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/gifs/loading.gif'),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Task 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Task 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Task 3'),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
+/// End of the Isolates Screen \\\
