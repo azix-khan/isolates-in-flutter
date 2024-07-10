a3ff235740859d7ed7e820c2d70f05573fb1bb54
azix khan <azixkhan.55@gmail.com>
2024-07-10 15:31:34 +0500

    created isolates sceen

---
+++ lib/isolates_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IsolatesScreen extends StatefulWidget {
+  const IsolatesScreen({super.key});
+
+  @override
+  State<IsolatesScreen> createState() => _IsolatesScreenState();
+}
+
+class _IsolatesScreenState extends State<IsolatesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
